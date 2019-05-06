66b452646ff60130d351bcd25680e89c1541c830
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-06 22:34:32 +0100

    Update easyplotting.jl

--- src/easyplotting.jl
+++ src/easyplotting.jl
@@ -1,6 +1,6 @@
 module easyplotting ## Define easyplotting module
 
 #include(joinpath(pathof(easyplotting)[1:end-12], "easyheatmap.jl")) ## Include easyheatmap.jl script for function easyheatmap()
-include("easyheatmap.jl")
+cd(joinpath(pathof(easyplotting)[1:end-15]) && include("easyheatmap.jl")
 
 end #module
