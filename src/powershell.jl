cc1e89a7566c582b080ab8089d0aa27486e6f690
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-07 19:06:54 +0100

    Update powershell.jl

--- src/powershell.jl
+++ src/powershell.jl
@@ -25,6 +25,14 @@ if success(`powershell.exe where.exe /R C: python3`) == false
     end
     
     cd(pathof(easyplotting)[1:end-15])
+    if success(`powershell.exe choco install python3 --confirm`) == true
+        run(`powershell.exe choco install python3 --confirm`)
+    else
+        run(`powershell.exe ./python3_non_admin_install.ps1`)
+    end
+end
+
+if success(`powershell.exe where.exe /R C: python3`) == true && success(`powershell.exe where.exe /R C: python37`) == false
     if chomp(read(`powershell.exe Get-ExecutionPolicy`, String)) == "Restricted"
         if success(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`) == true
             run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`)
@@ -32,12 +40,12 @@ if success(`powershell.exe where.exe /R C: python3`) == false
             run(`powershell.exe Set-ExecutionPolicy -Scope CurrentUser Bypass`)
         end
     end
-    
+
     cd(pathof(easyplotting)[1:end-15])
-    if success(`powershell.exe choco install python3 --confirm`) == true
-        run(`powershell.exe choco install python3 --confirm`)
+    if success(`powershell.exe choco upgrade python3 --confirm`) == true
+        run(`powershell.exe choco upgrade python3 --confirm`)
     else
-        run(`powershell.exe ./python3_non_admin_install.ps1`)
+        run(`powershell.exe ./python3_non_admin_upgrade.ps1`)
     end
 end
 
