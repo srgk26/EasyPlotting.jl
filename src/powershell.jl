9b5edd29a0a61d023adcee02544002c03c6042d9
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-07 18:59:11 +0100

    Update powershell.jl

--- src/powershell.jl
+++ src/powershell.jl
@@ -15,6 +15,32 @@ if success(`powershell.exe where.exe choco`) == false
     end
 end
 
+if success(`powershell.exe where.exe /R C: python3`) == false
+    if chomp(read(`powershell.exe Get-ExecutionPolicy`, String)) == "Restricted"
+        if success(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`) == true
+            run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`)
+        else
+            run(`powershell.exe Set-ExecutionPolicy -Scope CurrentUser Bypass`)
+        end
+    end
+    
+    cd(pathof(easyplotting)[1:end-15])
+    if chomp(read(`powershell.exe Get-ExecutionPolicy`, String)) == "Restricted"
+        if success(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`) == true
+            run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`)
+        else
+            run(`powershell.exe Set-ExecutionPolicy -Scope CurrentUser Bypass`)
+        end
+    end
+    
+    cd(pathof(easyplotting)[1:end-15])
+    if success(`powershell.exe choco install python3 --confirm`) == true
+        run(`powershell.exe choco install python3 --confirm`)
+    else
+        run(`powershell.exe ./python3_non_admin_install.ps1`)
+    end
+end
+
 if success(`powershell.exe where.exe /R C: julia`) == false
     run(`powershell.exe choco install julia --confirm`)
 end
