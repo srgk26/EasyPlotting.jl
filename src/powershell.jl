4ddbb4a2ceeb88ee4b9c43d5f37b19fb3d6378bc
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-07 17:49:45 +0100

    Update powershell.jl

--- src/powershell.jl
+++ src/powershell.jl
@@ -1,2 +1,17 @@
-if chomp(read(`powershell.exe Get-ExecutionPolicy`, String)) == "Restricted"
+if success(`powershell.exe where.exe choco`) == false
+    if chomp(read(`powershell.exe Get-ExecutionPolicy`, String)) == "Restricted"
+        if success(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`) == true
+            run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process`)
+        else
+            run(`powershell.exe Set-ExecutionPolicy -Scope CurrentUser Bypass`)
+        end
+    end
+    
+    cd(pathof(easyplotting)[1:end-15])
+    if success(`powershell.exe ./choco_install_Windows_admin.ps1`) == true
+        run(`powershell.exe ./choco_install_Windows_admin.ps1`)
+    else
+        run(`powershell.exe ./choco_install_Windows_non_admin.ps1`)
+    end
+end
 
