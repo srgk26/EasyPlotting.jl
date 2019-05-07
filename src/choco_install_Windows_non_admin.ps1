d9db960b030263fddd1bc80488eee9d888a41b7f
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-07 17:21:41 +0100

    Update choco_install_Windows_non_admin.ps1

--- src/choco_install_Windows_non_admin.ps1
+++ src/choco_install_Windows_non_admin.ps1
@@ -1,6 +1,8 @@
 $InstallDir='C:\ProgramData\chocoportable' ## Change install directory for non-admin users
 $env:ChocolateyInstall="$InstallDir"
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+## Install some basic programmes
 choco install puppet-agent.portable -y
 choco install ruby.portable -y
 choco install git.commandline -y
