ae40eaa327e8964d367d50b101f6c169b6846a10
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-08 16:31:52 +0100

    Delete python3_non_admin_upgrade.ps1

--- src/python3_non_admin_upgrade.ps1
+++
@@ -1,6 +0,0 @@
-$InstallDir='C:\ProgramData\chocoportable' ## Change install directory for non-admin users
-$env:ChocolateyInstall="$InstallDir"
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-## Install Python3
-choco upgrade python3 --confirm
