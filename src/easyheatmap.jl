b27c499a440dd4081cabdafb79e750443cc0a443
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-08 19:00:16 +0100

    Update easyheatmap.jl

--- src/easyheatmap.jl
+++ src/easyheatmap.jl
@@ -122,8 +122,11 @@ function easyheatmap() ## Defining function to be called by user
 			end
 		
 			if success(`powershell.exe where.exe python`) == false
+				run(`powershell.exe python3_install_nonadmin.ps1`)
 			elseif success(`powershell.exe where.exe python3`) == true && chomp(read(`powershell.exe where.exe python`, String))[end-12] !== "3"
+				run(`powershell.exe python3_install_nonadmin.ps1`)
 			elseif success(`powershell.exe where.exe python3`) == true && chomp(read(`powershell.exe where.exe python`, String))[end-12] == "3" && chomp(read(`powershell.exe where.exe python`, String))[end-11] !== "7"
+				run(`powershell.exe python3_install_nonadmin.ps1`)
 			end
 		
 			if in("seaborn", joinpath(readdir(`powershell.exe where.exe python`)[1:end-10], "lib/site-packages")) == false
@@ -132,58 +135,11 @@ function easyheatmap() ## Defining function to be called by user
         	end
 		
 			if success(`powershell.exe where.exe julia`) == false
-            	run(`powershell.exe choco install julia --confirm`)
+            	run(`powershell.exe julia_install_nonadmin.ps1 `)
         	end
 		end
 	
-
-								
-
-		
-		
-		
-	
-		
-		
-
-        if success(`powershell.exe where.exe choco`) == false
-            if admin == "Y" || "y"
-                run(`powershell.exe choco_install_admin.ps1`)
-            elseif admin == "N" || "n"
-                run(`powershell.exe choco_install_nonadmin.ps1`)
-            end
-            run(`powershell.exe refreshenv`)
-        end
-
-        if success(`powershell.exe where.exe python`) == false
-            if admin == "Y" || "y"
-                run(`powershell.exe choco install python3 --confirm`)
-            elseif admin == "N" || "n"
-                run(`powershell.exe python3_install_nonadmin.ps1`)
-            end
-            run(`powershell.exe refreshenv`)
-        end
-
-        if success(`powershell.exe where.exe python3`) == true && chomp(read(`powershell.exe where.exe python`, String))[end-12] == "3" && chomp(read(`powershell.exe where.exe python`, String))[end-11] !== "7"
-            if admin == "Y" || "y"
-                run(`powershell.exe choco upgrade python3 --confirm`)
-            elseif admin == "N" || "n"
-                run(`powershell.exe python3_install_nonadmin.ps1`)
-            end
-            run(`powershell.exe refreshenv`)
-        end
-
-    if in("seaborn", joinpath(readdir(`powershell.exe where.exe python`)[1:end-10], "lib/site-packages")) == false
-            run(`pip3 install --upgrade pip`)
-            run(`pip3 install seaborn`)
-        end
-
-
-        if success(`powershell.exe where.exe julia`) == false
-            run(`powershell.exe choco install julia --confirm`)
-        end
-	
-	run(`powershell.exe refreshenv`)
+		run(`powershell.exe refreshenv`)
     end
 
     #### Install additional required Julia installations if not already installed
