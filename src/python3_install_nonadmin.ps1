3828e8091af96753cba313e24c5804959d8152bd
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-08 16:06:29 +0100

    Create python3_install_nonadmin.ps1

---
+++ src/python3_install_nonadmin.ps1
@@ -0,0 +1,6 @@
+Register-PackageSource -Name Nuget -Location "http://www.nuget.org/api/v2" –ProviderName Nuget -Trusted
+find-package gitforwindows | install-package -Scope CurrentUser
+find-package python | install-package -Scope CurrentUser
+get-package python | % source
+[System.Environment]::GetEnvironmentVariable('Path', 'User')
+[System.Environment]::SetEnvironmentVariable('Path', $newPathInSingleStringSeparatedByColumn, 'User')
