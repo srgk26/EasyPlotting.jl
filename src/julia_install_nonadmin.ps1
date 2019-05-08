ed91ec811a4200bc12dc3d0d8873246defaa4671
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-08 18:56:07 +0100

    Update julia_install_nonadmin.ps1

--- src/julia_install_nonadmin.ps1
+++ src/julia_install_nonadmin.ps1
@@ -1,6 +1,6 @@
 $InstallDir='C:\ProgramData\chocoportable' ## Change install directory for non-admin users
 $env:ChocolateyInstall="$InstallDir"
-((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) ## Install Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) ## Install Chocolatey
 
 ## Install Julia
 choco install julia --confirm
