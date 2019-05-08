4635e85124b79e8bad8d5e293de8c6dcb8b5774b
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-08 16:54:17 +0100

    Update and rename choco_install_Windows_admin.ps1 to choco_admininstall.ps1

--- src/choco_admininstall.ps1
+++ src/choco_admininstall.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-## Install some basic programmes
+## Install basic packages
 choco install puppet-agent.portable -y
 choco install ruby.portable -y
 choco install git.commandline -y
