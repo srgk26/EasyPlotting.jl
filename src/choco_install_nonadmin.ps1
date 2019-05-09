0eb66c72b3d0deb6ed27a902e810caa6ae6d8de3
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-09 14:23:56 +0100

    Delete choco_install_nonadmin.ps1

--- src/choco_install_nonadmin.ps1
+++
@@ -1,8 +0,0 @@
-$InstallDir='C:\ProgramData\chocoportable' ## Change install directory for non-admin users
-$env:ChocolateyInstall="$InstallDir"
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) ## Install Chocolatey
-
-## Install basic packages
-choco install puppet-agent.portable -y
-choco install ruby.portable -y
-choco install git.commandline -y
