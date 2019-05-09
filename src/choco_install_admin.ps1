622cd81b8aaae3605c97d958fa107d8aa9883629
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-09 18:42:05 +0100

    Update choco_install_admin.ps1

--- src/choco_install_admin.ps1
+++ src/choco_install_admin.ps1
@@ -1,6 +1,1 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-## Install basic packages
-choco install puppet-agent.portable -y
-choco install ruby.portable -y
-choco install git.commandline -y
