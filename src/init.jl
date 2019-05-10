89331f86f6a843d464a18d8ae7a579890614259b
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-10 13:45:20 +0100

    Update init.jl

--- src/init.jl
+++ src/init.jl
@@ -17,13 +17,22 @@ function init()
         end
 
         ## Install seaborn python3 package if not already installed
-        if in("seaborn", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/"))) == false
+        try ## Trying try/catch blocks in the event something goes wrong in the user's computer, the process would be executed anyway
+            if in("seaborn", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/"))) == false
+                run(`pip3 install --upgrade pip`)
+                run(`pip3 install seaborn`)
+            end
+        catch
             run(`pip3 install --upgrade pip`)
             run(`pip3 install seaborn`)
         end
 
         ## Adding pyqt matplotlib backend for compatibility with seaborn plots
-        if in("backend_qt5.py", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/matplotlib/backends"))) == false
+        try
+            if in("backend_qt5.py", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/matplotlib/backends"))) == false
+                Conda.add("pyqt")
+            end
+        catch
             Conda.add("pyqt")
         end
 
@@ -50,38 +59,51 @@ function init()
         end
 
         ## Install seaborn python3 package if not already installed
-        if in("seaborn", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/"))) == false
+        try
+            if in("seaborn", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/"))) == false
+                run(`pip3 install --upgrade pip`)
+                run(`pip3 install seaborn`)
+            end
+        catch
             run(`pip3 install --upgrade pip`)
             run(`pip3 install seaborn`)
         end
 
         ## Adding pyqt matplotlib backend for compatibility with seaborn plots
-        if in("backend_qt5.py", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/matplotlib/backends"))) == false
+        try
+            if in("backend_qt5.py", readdir(joinpath(chomp(read(`which python3`, String))[1:end-11], "lib/python3.7/site-packages/matplotlib/backends"))) == false
+                Conda.add("pyqt")
+            end
+        catch
             Conda.add("pyqt")
         end
 
         ## Install julia in PATH if not already installed
-        if success(`which julia`) == false
-            if success(`which apt`) == true
-                run(`sudo apt-get update`)
-                run(`sudo apt-get dist-upgrade`)
-                run(`sudo apt-get install julia`)
-            elseif success(`which dnf`) == true
-                run(`sudo dnf update`)
-                run(`sudo yum update`)
-                run(`sudo dnf copr enable nalimilan/julia`)
-                run(`sudo yum install julia`)
-            elseif success(`which dnf`) == false && success(`which yum`) == true
-                run(`sudo yum update`)
-                run(`sudo yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/nalimilan/julia/repo/epel-7/nalimilan-julia-epel-7.repo`)
-                run(`sudo yum install julia`)
-            elseif success(`which pkg`) == true
-                run(`pkg upgrade`)
-                run(`pkg install julia`)
-            elseif success(`which pacman`) == true
-                run(`pacman -Syu`)
-                run(`pacman -S julia`)
+        try
+            if success(`which julia`) == false
+                if success(`which apt`) == true
+                    run(`sudo apt-get update`)
+                    run(`sudo apt-get dist-upgrade`)
+                    run(`sudo apt-get install julia`)
+                elseif success(`which dnf`) == true
+                    run(`sudo dnf update`)
+                    run(`sudo yum update`)
+                    run(`sudo dnf copr enable nalimilan/julia`)
+                    run(`sudo yum install julia`)
+                elseif success(`which dnf`) == false && success(`which yum`) == true
+                    run(`sudo yum update`)
+                    run(`sudo yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/nalimilan/julia/repo/epel-7/nalimilan-julia-epel-7.repo`)
+                    run(`sudo yum install julia`)
+                elseif success(`which pkg`) == true
+                    run(`pkg upgrade`)
+                    run(`pkg install julia`)
+                elseif success(`which pacman`) == true
+                    run(`pacman -Syu`)
+                    run(`pacman -S julia`)
+                end
             end
+        catch
+            nothing ## If Julia installation in path is unsuccessful, do nothing as Julia would already be installed by user anyway.
         end
     end
 
@@ -117,13 +139,22 @@ function init()
             end
 
             ## Install seaborn python3 package if not already installed
-            if in("seaborn", readdir(joinpath(chomp(read(`powershell.exe where.exe python`, String))[1:end-10], "lib/site-packages"))) == false
+            try
+                if in("seaborn", readdir(joinpath(chomp(read(`powershell.exe where.exe python`, String))[1:end-10], "lib/site-packages"))) == false
+                    run(`pip3 install --upgrade pip`)
+                    run(`pip3 install seaborn`)
+                end
+            catch
                 run(`pip3 install --upgrade pip`)
                 run(`pip3 install seaborn`)
             end
 
             ## Adding pyqt matplotlib backend for compatibility with seaborn plots
-            if in("backend_qt5.py", readdir(joinpath(chomp(read(`powershell.exe where.exe python`, String))[1:end-10], "lib/site-packages/matplotlib/backends"))) == false
+            try
+                if in("backend_qt5.py", readdir(joinpath(chomp(read(`powershell.exe where.exe python`, String))[1:end-10], "lib/site-packages/matplotlib/backends"))) == false
+                    Conda.add("pyqt")
+                end
+            catch
                 Conda.add("pyqt")
             end
 
@@ -136,20 +167,33 @@ function init()
         ## For non-admin users, only critical software applications will be installed as non-critical additional software installations are suboptimal
         elseif admin == "N" || admin == "n"
             ## Install python3.7 if not already installed
-            if in("Python", readdir(joinpath(homedir(), "AppData/Local/Programs"))) == false
-                run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process; python3_install_nonadmin.ps1`)
-            elseif in("Python", readdir(joinpath(homedir(), "AppData/Local/Programs"))) == true && in("Python37", readdir(joinpath(homedir(), "AppData/Local/Programs/Python"))) == false
+            try
+                if in("Python", readdir(joinpath(homedir(), "AppData/Local/Programs"))) == false
+                    run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process; python3_install_nonadmin.ps1`)
+                elseif in("Python", readdir(joinpath(homedir(), "AppData/Local/Programs"))) == true && in("Python37", readdir(joinpath(homedir(), "AppData/Local/Programs/Python"))) == false
+                    run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process; python3_install_nonadmin.ps1`)
+                end
+            catch
                 run(`powershell.exe Set-ExecutionPolicy Bypass -Scope Process; python3_install_nonadmin.ps1`)
             end
 
             ## Install seaborn python3 package if not already installed
-            if in("seaborn", readdir(joinpath(homedir(), "AppData/Local/Programs/Python/Python37/lib/site-packages"))) == false
+            try
+                if in("seaborn", readdir(joinpath(homedir(), "AppData/Local/Programs/Python/Python37/lib/site-packages"))) == false
+                    run(`pip3 install --upgrade pip`)
+                    run(`pip3 install seaborn`)
+                end
+            catch
                 run(`pip3 install --upgrade pip`)
                 run(`pip3 install seaborn`)
             end
             
             ## Adding pyqt matplotlib backend for compatibility with seaborn plots
-            if in("backend_qt5.py", readdir(joinpath(homedir(), "AppData/Local/Programs/Python/Python37/lib/site-packages"))) == false
+            try
+                if in("backend_qt5.py", readdir(joinpath(homedir(), "AppData/Local/Programs/Python/Python37/lib/site-packages"))) == false
+                    Conda.add("pyqt")
+                end
+            catch
                 Conda.add("pyqt")
             end
             
@@ -160,7 +204,11 @@ function init()
     end
 
     ## Installing Electron browser (and renaming to Julia.app)
-    if in("Julia.app", readdir(joinpath(pathof(Blink)[1:end-12], "deps"))) == false
+    try
+        if in("Julia.app", readdir(joinpath(pathof(Blink)[1:end-12], "deps"))) == false
+            Blink.AtomShell.install()
+        end
+    catch
         Blink.AtomShell.install()
     end
 end
