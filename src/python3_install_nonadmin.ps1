4d35561e88b6e30c6e38568456f1e8ab732317b1
S R Gokul Krishnan <49290594+srgk26@users.noreply.github.com>
2019-05-10 18:42:12 +0100

    Delete python3_install_nonadmin.ps1

--- src/python3_install_nonadmin.ps1
+++
@@ -1,7 +0,0 @@
-Register-PSRepository -Default -InstallationPolicy Trusted
-Register-PackageSource -Name Nuget -Location "http://www.nuget.org/api/v2" –ProviderName Nuget -Trusted
-find-package gitforwindows | install-package -Scope CurrentUser
-find-package python | install-package -Scope CurrentUser
-get-package python | % source
-[System.Environment]::GetEnvironmentVariable('Path', 'User')
-[System.Environment]::SetEnvironmentVariable('Path', $newPathInSingleStringSeparatedByColumn, 'User')
