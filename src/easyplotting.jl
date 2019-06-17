3853cd01ad25ffb35d092ac1ff973117e95fd955
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-06-17 10:28:45 +0100

    Update easyplotting.jl

--- src/easyplotting.jl
+++ src/easyplotting.jl
@@ -5,5 +5,12 @@ using Blink, Interact, XLSX, CSV, DelimitedFiles, DataFrames, Plots, PlotlyJS, S
 ## Include all source scripts
 include(joinpath(pathof(easyplotting)[1:end-15], "mainpage.jl")) ## Include 'mainpage.jl'
 include(joinpath(pathof(easyplotting)[1:end-15], "Heatmap/easyheatmap.jl")) ## Include 'easyheatmap.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "Scatterplot/easyscatterplot.jl")) ## Include 'easyscatterplot.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "Linegraph/easylinegraph.jl")) ## Include 'easylinegraph.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "Histogram/easyhistogram.jl")) ## Include 'easyhistogram.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "Barchart/easybarchart.jl")) ## Include 'easybarchart.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "BoxandWhisker/easyboxandwhisker.jl")) ## Include 'easyboxandwhisker.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "Piechart/easypiechart.jl")) ## Include 'easypiechart.jl'
+include(joinpath(pathof(easyplotting)[1:end-15], "Violinplot/easyviolinplot.jl")) ## Include 'easyviolinplot.jl'
 
 end #module
