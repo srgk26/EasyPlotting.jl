def3eede8abbe66e1829cb7f7f72c2d6e286ebc1
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-09-27 18:37:40 +0100

    Update easyscatterplot3d.jl

--- src/easyscatterplot3d.jl
+++ src/easyscatterplot3d.jl
@@ -66,11 +66,6 @@ function easyscatterplot3d()
                 DataFrames.deleterows!(df, 1) ## Deleting row 1 of df
             end
 
-            ## Alert if sheet name is not entered for excel .xlsx files
-            if (easyscatterplot3d_inputsFn["easyscatterplot3d_file"][]::String)[end-3:end] == "xlsx" && easyscatterplot3d_inputsFn["easyscatterplot3d_sheet"][]::String == ""
-                @js_ w alert("Excel .xlsx sheet name not entered. Kindly enter the sheet name and try again.")
-            end
-
             ## Plot scatterplot3d
             StatsPlots.plotlyjs() ## Using PLotlyJS backend
             if easyscatterplot3d_inputsFn["easyscatterplot3d_size1"][]::String == "" ## If no user-input for plot size
@@ -111,7 +106,12 @@ function easyscatterplot3d()
                 end
             end
         catch
-            @js_ w alert("Oops! Something had gone wrong. Could it be that your user input dataset is of the wrong format?")
+            ## Alert if sheet name is not entered for excel .xlsx files
+            if (easyscatterplot3d_inputsFn["easyscatterplot3d_file"][]::String)[end-3:end] == "xlsx" && easyscatterplot3d_inputsFn["easyscatterplot3d_sheet"][]::String == ""
+                @js_ w alert("Excel .xlsx sheet name not entered. Kindly enter the sheet name and try again.")
+            else
+                @js_ w alert("Oops! Something had gone wrong. Could it be that your user input dataset is of the wrong format?")
+            end
         end
     end
 end #function easyscatterplot3d()
