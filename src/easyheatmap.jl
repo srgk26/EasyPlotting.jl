a4e41b3f4f7ef2637fbff0b5cc25a7949223416b
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-09-27 18:29:17 +0100

    Update easyheatmap.jl

--- src/easyheatmap.jl
+++ src/easyheatmap.jl
@@ -436,15 +436,15 @@ function easyheatmap()
                 DataFrames.deleterows!(df, 1) ## Deleting row 1 of df
             end
 
-            ## Alert if sheet name is not entered for excel .xlsx files
-            if (easyheatmap_inputsFn["easyheatmap_file"][]::String)[end-3:end] == "xlsx" && easyheatmap_inputsFn["easyheatmap_sheet"][]::String == ""
-                @js_ w alert("Excel .xlsx sheet name not entered. Kindly enter the sheet name and try again.")
-            end
-
             ## Plot heatmap
             easyheatmap_events() ## When easyheatmap_plot_button is pressed, easyheatmap_events() is executed.
         catch
-            @js_ w alert("Oops! Something had gone wrong. Could it be that your user input dataset is of the wrong format?")
+            ## Alert if sheet name is not entered for excel .xlsx files
+            if (easyheatmap_inputsFn["easyheatmap_file"][]::String)[end-3:end] == "xlsx" && easyheatmap_inputsFn["easyheatmap_sheet"][]::String == ""
+                @js_ w alert("Excel .xlsx sheet name not entered. Kindly enter the sheet name and try again.")
+            else
+                @js_ w alert("Oops! Something had gone wrong. Could it be that your user input dataset is of the wrong format?")
+            end
         end
     end
 
