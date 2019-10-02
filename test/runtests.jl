af39bfa5d727601a825e9ca166c855d2ffa62da3
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-10-02 05:21:53 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,14 @@
-using EasyPlotting; EasyPlotting.easymain() ## Test EasyPlotting.jl precompilation
+using EasyPlotting ## Test EasyPlotting precompilation
+
+## Test all functions under EasyPlotting.jl
+EasyPlotting.easymain()
+EasyPlotting.easybarchart()
+EasyPlotting.easyboxandwhisker()
+EasyPlotting.easyheatmap()
+EasyPlotting.easyhistogram()
+EasyPlotting.easylinegraph()
+EasyPlotting.easypiechart()
+EasyPlotting.easyscatterplot2d()
+EasyPlotting.easyscatterplot3d()
+EasyPlotting.easystripplot()
+EasyPlotting.easyviolinplot()
