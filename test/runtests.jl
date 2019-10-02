d2cf1f1d4fa54bdf7bc79b6c341175fe25c5545e
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-10-02 01:06:51 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,3 @@
-using EasyPlotting
-EasyPlotting.easymain()
+ENV["PYTHON"]="" ## Configuring PyCall to use Julia-specific Python distribution via the Conda.jl package
+using Pkg; Pkg.build("PyCall") ## Re-building PyCall
+using EasyPlotting; EasyPlotting.easymain() ## Run EasyPlotting.jl
