8550f835fdf17d9b593d1a0b4d0df8eb0f493369
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-10-10 01:51:12 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,23 @@
+## Test successful precompilation of EasyPlotting and loading of EasyPlotting functions
+using EasyPlotting, Test
+@test_nowarn EasyPlotting.easymain()
+@test_nowarn EasyPlotting.easybarchart()
+@test_nowarn EasyPlotting.easyboxandwhisker()
+@test_nowarn EasyPlotting.easyheatmap()
+@test_nowarn EasyPlotting.easyhistogram()
+@test_nowarn EasyPlotting.easylinegraph()
+@test_nowarn EasyPlotting.easypiechart()
+@test_nowarn EasyPlotting.easyscatterplot2d()
+@test_nowarn EasyPlotting.easyscatterplot3d()
+@test_nowarn EasyPlotting.easystripplot()
+@test_nowarn EasyPlotting.easyviolinplot()
+
 ## Test successfull installation of python3's seaborn package using Conda.jl
 ENV["PYTHON"]="" ## Reset python3 PATH to set Julia-specific python3 version maintained by Conda.jl
 using Pkg; Pkg.build("PyCall") ## Re-build PyCall
 
-using Blink, Interact, Seaborn, Conda, Test ## Use packages specific for testing
+## Use packages specific for additional testing
+using Blink, Interact, Seaborn, Conda ## 'Using Seaborn' should automatically add python3's seaborn via Conda.jl
 
 ## Include src scripts for testing
 include("../src/easymain.jl")
@@ -29,3 +44,6 @@ include("../src/easyviolinplot.jl")
 @test_nowarn easyscatterplot3d()
 @test_nowarn easystripplot()
 @test_nowarn easyviolinplot()
+
+## Test successful plotting with Julia's Seaborn built using python3' seaborn installed via Conda.jl
+@test_nowarn Seaborn.plot(rand(5,5))
